f258d81661681684932fddd0817242fd6a4eb366
aljawaid <jawaid_imran@hotmail.com>
2022-10-26 21:06:02 +0100

    Initial commit

---
+++ Plugin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Kanboard\Plugin\PluginNameExampleStudlyCaps;
+
+use Kanboard\Core\Plugin\Base;
+use Kanboard\Core\Translator;
+
+class Plugin extends Base
+{
+    public function initialize()
+    {
+        // Template - Override name should be camelCase e.g. pluginNameExampleCamelCase
+        $this->template->setTemplateOverride('action/index', 'pluginNameExampleCamelCase:action/index');
+
+        // CSS - Asset Hook - keep filename lowercase
+        $this->hook->on('template:layout:css', array('template' => 'plugins/PluginNameExampleStudlyCaps/Assets/css/plugin-name.css'));
+
+        // JS - Asset Hook - keep filename lowercase
+        $this->hook->on('template:layout:js', array('template' => 'plugins/PluginNameExampleStudlyCaps/Assets/js/plugin-name.js'));
+
+        // Board Views - Template Hook - Override name should start lowercase e.g. pluginNameExampleCamelCase
+        $this->template->hook->attach('template:project-header:view-switcher-before-project-overview', 'pluginNameExampleCamelCase:project_header/actions');
+    }
+
+    public function onStartup()
+    {
+        Translator::load($this->languageModel->getCurrentLanguage(), __DIR__.'/Locale');
+    }
+
+    public function getPluginName()
+    {
+        return 'Plugin Name';
+    }
+
+    public function getPluginDescription()
+    {
+        return t('description text');
+    }
+
+    public function getPluginAuthor()
+    {
+        return 'aljawaid';
+    }
+
+    public function getPluginVersion()
+    {
+        return '1.0.0';
+    }
+
+    public function getCompatibleVersion()
+    {
+        // Examples:
+        // >=1.0.37
+        // <1.0.37
+        // <=1.0.37
+        return '1.0.37';
+    }
+
+    public function getPluginHomepage()
+    {
+        return 'https://github.com/aljawaid/url';
+    }
+}
